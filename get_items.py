91fb41774689a2dc9cc7ba2fd2d63beca856e384
sudoHub <i.am.ableton1@gmail.com>
2020-10-06 11:53:26 -0700

    finished filling out form up to continue checkout buttom

--- get_items.py
+++ get_items.py
@@ -10,7 +10,7 @@ driver = Chrome(options=opts)   # set driver environment to Chrome browser
 
 driver.get('https://www.bladehq.com/item--CIVIVI-Elementum-Liner-Lock-Knife--114184')
 
-#find the "add to cart button" and click
+#find the "add to cart button" and click // Saving to buy_button for element testing
 buy_button = driver.find_element_by_name("submit").click()
 
 #find the checkout as guest button and click it
@@ -22,7 +22,7 @@ name = 'Full Name'
 ship_address = '1234 Forever Way'
 city = 'Los Angles'
 zip = '90210'
-phone_num = '5553455'
+phone_num = '801-555-3455'
 email = 'bot@boter.com'
 
 #insert full name
@@ -32,9 +32,18 @@ driver.find_element_by_id("scity").send_keys(city)
 driver.find_element_by_id("szip").send_keys(zip)
 
 # drop down menu for country
+driver.find_element_by_xpath("//*[@id='scountry']/option[ text() = 'United States']").click()
+
+# Drop down menu for State
+driver.find_element_by_xpath("//*[@id='sstate']/option[text() = 'Utah']").click()
+
 # Phone
+driver.find_element_by_xpath("//*[@id='sphone']").send_keys(phone_num)
+
 # email
+driver.find_element_by_xpath("//*[@id='email_addr']").send_keys(email)
 
 # Click Continue Checkout button
+driver.find_element_by_xpath("//*[@id='checkout_new_addrs_form']/div[2]/div/div/div[2]/button").click()
 
 print("Program Complete.")
